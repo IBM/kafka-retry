765c0b46201f7d73a2aea6f79c6bfea82db9ebb0
boonware <josefharte@gmail.com>
2020-11-19 14:09:16 +0000

    Update image tagging

--- pipeline/push.sh
+++ pipeline/push.sh
@@ -3,15 +3,18 @@
 # SPDX-License-Identifier: Apache2.0
 #
 
-readonly REPO+URL=https://hub.docker.com/repository/docker/ibmcom/kafka-retry
+set -e
+
+readonly REPO_URL=https://hub.docker.com/repository/docker/ibmcom/kafka-retry
 readonly DOCKER_ACCOUNT=ibmcom
 readonly REPO_NAME=kafka-retry
+readonly IMAGE_TAG=${TRAVIS_TAG#v}
 
 if [[ ! -z ${TRAVIS_TAG} ]]; then
   echo "[INFO] Tagging and pushing Docker image to ${REPO_URL} ..."
-  docker tag ${REPO_NAME}:${TRAVIS_COMMIT} ${DOCKER_ACCOUNT}/${REPO_NAME}:${TRAVIS_TAG}
+  docker tag ${REPO_NAME}:${TRAVIS_COMMIT} ${DOCKER_ACCOUNT}/${REPO_NAME}:${IMAGE_TAG}
   echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
-  docker push ${DOCKER_ACCOUNT}/${REPO_NAME}:${TRAVIS_TAG}
+  docker push ${DOCKER_ACCOUNT}/${REPO_NAME}:${IMAGE_TAG}
   echo "[INFO] Image push complete"
 else
   echo '[ERROR] Cannot push image as Git tag is empty'
