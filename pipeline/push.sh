f352bf361387ed89d486c0ab0cb1ff4597733cf2
boonware <josefharte@gmail.com>
2020-11-19 14:57:19 +0000

    Fix push script

--- pipeline/push.sh
+++ pipeline/push.sh
@@ -8,18 +8,19 @@ set -e
 readonly REPO_URL=https://hub.docker.com/repository/docker/ibmcom/kafka-retry
 readonly DOCKER_ACCOUNT=ibmcom
 readonly REPO_NAME=kafka-retry
+readonly VERSION=${TRAVIS_TAG#v}
 readonly MAJOR=$(echo ${VERSION} | cut -d. -f1)
 readonly MINOR=$(echo ${VERSION} | cut -d. -f2)
 readonly PATCH=$(echo ${VERSION} | cut -d. -f3)
 
 tag_and_push() {
   local tag=${1}
+  echo "[INFO] Pushing ${DOCKER_ACCOUNT}/${REPO_NAME}:${tag} ..."
   docker tag ${REPO_NAME}:${TRAVIS_COMMIT} ${DOCKER_ACCOUNT}/${REPO_NAME}:${tag}
   docker push ${DOCKER_ACCOUNT}/${REPO_NAME}:${tag}
 }
 
 if [[ ! -z ${TRAVIS_TAG} ]]; then
-  echo "[INFO] Tagging and pushing Docker image to ${REPO_URL} ..."
   echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
   tag_and_push latest
   tag_and_push ${MAJOR}.${MINOR}.${PATCH}
